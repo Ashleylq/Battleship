b654716c1b51bfa68fd3cc15a72af8e2821988ab
Ashleylq <ironmanashley@gmail.com>
2025-05-24 06:55:36 +0000

    Add ship class and gameboard placing functions

---
+++ index.js
@@ -0,0 +1,92 @@
+class Ship{
+    constructor(length){
+        this.length = length;
+        this.sunk = false;
+        this.hits = 0;
+    }
+    hit(){
+        this.hits++;
+        this.isSunk();
+    }
+
+    isSunk(){
+        if(this.hits === this.length){
+            this.sunk = true;
+        }
+    }
+}
+
+class GameBoard{
+    constructor(){
+        this.board = this.createBoard(10, 10);
+    }
+    createBoard(row, col){
+        let board = [];
+        for(let i = 1; i <= row; i++){
+            let row = [];
+            for(let i = 1; i <= col; i++){
+                let col = {
+                    hasShip : false,
+                    ship : 'none',
+                    status : 'active',
+                }
+                row.push(col);
+            }
+            board.push(row);
+        }
+        return board
+    }
+
+    #randomNumber(num){
+        return Math.floor(Math.random() * num);
+    }
+
+    #canPlaceShip(x, y, axis, length){
+        for(let i = 0; i < length; i++){
+            let xi = axis === 'x' ? x + i : x;
+            let yi = axis === 'y' ? y + i : y;
+            if(this.board[yi][xi].hasShip){return false};
+            if(xi >= 10 || yi >= 10){return false};
+        }
+        return true;
+    }
+
+    placeRandomShip(length){
+      let placed = false;
+      while(!placed){
+        let axis = this.#randomNumber(2) === 1 ? 'x' : 'y';
+        let num1 = this.#randomNumber(11 - length);
+        let num2 = this.#randomNumber(10);
+        let x;
+        let y;
+        if(axis = 'x'){
+            x = num1;
+            y = num2;
+        }
+        else {
+            y = num1;
+            x = num2;
+        }
+        if(this.#canPlaceShip(x, y, axis, length)){
+            placed = true;
+            this.placeShip(x, y, axis, length, true);
+        }
+      }
+    }
+    placeShip(x, y, axis, length, random = false){
+        if(random === false){
+            if(!this.#canPlaceShip(x, y, axis, length)){
+                return 'Cant place ship here';
+            }
+        }
+        const ship = new Ship(length);
+        for(let i = 0; i < length; i++){
+            let xi = axis === 'x' ? x + i : x;
+            let yi = axis === 'y' ? y + i : y;
+            this.board[yi][xi].hasShip = true;
+            this.board[yi][xi].ship = ship;
+        }
+    }
+}
+
+export {Ship, GameBoard}
---
+++ index.test.js
@@ -0,0 +1,21 @@
+import { Ship, GameBoard } from ".";
+
+test('Registers a hit', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+})
+
+test('Sinks when hits match length', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit()
+    expect(ship.sunk).toBe(true);
+})
+
+test('Cannot place two ships in one place', () => {
+    let gameBoard = new GameBoard();
+    gameBoard.placeShip(3, 3, 'x', 4);
+    expect(gameBoard.placeShip(3, 3, 'x', 4)).toBe('Cant place ship here');
+})
